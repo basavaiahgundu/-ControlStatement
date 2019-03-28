6e69b69f8aff59e263c4ee820ef222cbd1af9969
basavaiahgundu <bus.apps@gmail.com>
2019-03-28 19:37:41 +0530

    5th project add

---
+++ src/org/scb/Count.java
@@ -0,0 +1,16 @@
+package org.scb;
+
+public class Count {
+public static void main(String[] args) {
+	int count=0;
+	for(int i=1;i<=12;i++)
+	{
+		if(i%2==0)
+		{
+			count++;
+		}
+	}
+System.out.println(count);
+}
+
+}
---
+++ src/org/scb/OddEven.java
@@ -0,0 +1,20 @@
+package org.scb;
+
+public class OddEven
+
+{
+	public static void main(String[] args) {
+		
+	
+int a=10;
+if(a%2==0)
+{
+	System.out.println("Even");
+}
+else 
+{
+	System.out.println("Odd");
+}
+}
+	
+}
